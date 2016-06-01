4e678fead6908043c6112bd16390b13b778e30b6
Aaditi Jain <aaditi@Aaditis-MacBook-Air.local>
2016-06-01 23:34:21 +0530

    initial commit

---
+++ app/src/main/java/com/example/aaditi/helloworld/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.aaditi.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.aaditi.helloworld.customerData;
+import com.google.gson.Gson;
+
+import com.google.gson.*;
+import org.json.*;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView obj = (TextView)findViewById(R.id.textview1);
+
+        customerData d1= new customerData("aaditi",33);
+        customerData d2= new customerData("Mayank",34);
+
+
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        String d1Str = gson.toJson( d1 );
+
+
+        obj.setText(d1Str);
+    }
+}
---
+++ app/src/main/java/com/example/aaditi/helloworld/customerData.java
@@ -0,0 +1,17 @@
+package com.example.aaditi.helloworld;
+
+/**
+ * Created by aaditi on 01/06/16.
+ */
+public class customerData {
+    String customerName ;
+    int customerId ;
+    int myArray[] = {3,4} ;
+
+    public customerData(String myName,int myId)
+    {
+        customerName = myName;
+        customerId = myId ;
+    }
+
+}
